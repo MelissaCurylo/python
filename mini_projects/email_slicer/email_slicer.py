4a3f9580a6a04d3cb52811a952b2604295c3c853
Melissa <MelissaCode01@outlook.com>
2022-10-02 19:31:33 -0400

    commit
    commit

--- mini_projects/email_slicer/email_slicer.py
+++ mini_projects/email_slicer/email_slicer.py
@@ -1,7 +1,7 @@
 while True:
     email = input("Please enter your email address: ").strip() # Obtaining user emaill address via input // strip() method is removing whitespace from beginning and end of string.
 
-    if email.count('@') == 1 and len(email[email.index('@')]) > 0 and len(email[email.index('@') + 1:]) > 3:
+    if email.count('@') == 1 and len(email[email.index('@')]) > 0 and len(email[email.index('@') + 1:]) > 5:
 
         username = email[:email.index('@')] # using index() method slice out username with range up until @ symbol then storing as username.
         domain = email[email.index('@')+1:] # same method for domain but setting range after the @ symbol. 
