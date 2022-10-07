cd461d7e842e6d8d5ccb103af221f5850ce352a6
Melissa <MelissaCode01@outlook.com>
2022-10-07 05:46:39 -0400

    commit
    commit

--- mini_projects/price_compare/resources/item.py
+++ mini_projects/price_compare/resources/item.py
@@ -1,8 +1,12 @@
-import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import items
+
+from sqlalchemy.exc import SQLAlchemyError
+from db import db
+from models import ItemModel
+from schemas import ItemSchema, ItemUpdateSchema
+
 
 
 blp = Blueprint("Items", "items", description="Operations on items.")
@@ -10,56 +14,48 @@ blp = Blueprint("Items", "items", description="Operations on items.")
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+
+    @blp.response(200, ItemSchema)
     def get (self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        return item
+
+
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted."}
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Deleting an item is not implemented.")
 
-    def put(self, item_id):
-        item_data = request.get_data()
 
-        # NEED MORE VALIDATION HERE: price is a number and optionals
 
-        if "price" not in item_data or "name" not in item_data:
-            abort(400, message="Bad request. Check 'price' and 'name' are included in the JSON payload.")
-            
-            try: 
-                item = items[item_id]
-                item |= item_data
-                return item
 
-            except KeyError:
-                abort(404, message="Item not found.")
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
+    def put(self, item_data, item_id):
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Updating an item is not implemented.")
+
+
+
 
 
 @blp.route("/item")
 class ItemList(MethodView):
+
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        pass
+        # return items.values() # returns a list of items not object of items **************************************CORRECT THIS
 
-    def post(self):
-        item_data = request.get_json()
 
-        # NEED TO VALIDATE DATA TYPES 
-
-        if (
-            "price" not in item_data or "store_id" not in item_data or "name" not in item_data):
-                abort(400, message="Bad request. Check 'price', 'store_id', and 'name' are included in JSON payload.",)
-        for item in items.values():
-            if(
-                item_data["name"] == item["name"] and 
-                item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message=f"Item already exists.")
-
-        item_id = uuid.uuid4().hexitem = {**item_data, "id": item_id}
-        items[item_id] = item
-        return item
+    @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
+    def post(self, item_data):
+        item = ItemModel(**item_data)
+        
+        try:
+            db.session.add(item)
+            db.session.commit()
+            
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the item.")
--- mini_projects/price_compare/resources/store.py
+++ mini_projects/price_compare/resources/store.py
@@ -1,8 +1,12 @@
-import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import stores
+
+
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from db import db
+from models import StoreModel
+from schemas import StoreSchema
 
 
 blp = Blueprint("stores", __name__, description="Operations on stores.")
@@ -10,37 +14,41 @@ blp = Blueprint("stores", __name__, description="Operations on stores.")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.get_or_404(store_id)
+        return store
+
 
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {"message": "Store deleted."}
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.query.get_or_404(store_id)
+        raise NotImplementedError("Deleting a store is not implemented.")
 
 
 
 @blp.route("/store")
 class StoreList(MethodView):
+
+    @blp.response(200, StoreSchema(many=True)) # creating an instance of item schema with many.
     def get(self):
-        return {"stores": list(stores.values())}
+        pass
+        # return stores.values() # returns a list of items not object of items; Marshmallow can turn the list/dictionary or object into JSON.
 
-    def post(self):
-        store_data = request.get_json()
-        if "name" not in store_data:
-            abort(400, message="Bad request. Check 'name' is included in JSON payload.",
-            )
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(400, message=f"Store already exists.")
 
-        store_id = uuid.uuid4().hex
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
+    @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema) # response decorator needs to be deeper in the order, it sends documenation API structure so stack all on top of it.
+    def post(self, store_data):
+        store = StoreModel(**store_data)
+        
+        try:
+            db.session.add(store)
+            db.session.commit()
+
+        except IntegrityError:
+            abort(400, message="An error occurred creating the store.",)
+
+        except SQLAlchemyError:
+            abort(500, message="An error occurred creating the store.")
         
         return store
