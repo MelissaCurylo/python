13dec77822f16bb0a9deadcb52f86c3fd748db25
Melissa <MelissaCode01@outlook.com>
2022-11-12 03:37:06 -0500

    5-9_functions_practice
    5-9_functions_practice

--- 100_days_of_python/5-9_functions_practice.py
+++ 100_days_of_python/5-9_functions_practice.py
@@ -1,4 +1,18 @@
 """
+Creating functions
+"""
+
+# Define function
+def my_function():
+    print("Hello")
+    print("My name is Melissa")
+
+my_function()
+
+
+
+
+"""
 Practicing Pythonic functionality.
 """
 
@@ -9,14 +23,12 @@ Practicing Pythonic functionality.
     - Return Type: Absolute value
 """
 
-test_number_integer = abs(-99)
-test_number_float = abs(-99.199)
-test_number_complex = abs(-3 - 4j)
-raised_complex = abs(test_number_complex ** -2)
+def testing_abs(num):
+    test_number_integer = abs(-99) * num
+    test_number_float = abs(-99.199) 
+    test_number_complex = abs(-3 - 4j) 
+    raised_complex = abs(test_number_complex ** -2) 
+    print(test_number_integer, test_number_float, test_number_complex, raised_complex)
 
-print(test_number_integer)
-print(test_number_float)
-print(test_number_complex)
-print(raised_complex)
-
+print(testing_abs(2))
 
