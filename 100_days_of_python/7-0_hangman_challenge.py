7906f1b986ff622ee85e142a2c01089849dbca6f
Melissa <MelissaCode01@outlook.com>
2022-11-26 16:05:34 -0500

    7-0_hangman_challenge
    7-0_hangman_challenge

--- 100_days_of_python/7-0_hangman_challenge.py
+++ 100_days_of_python/7-0_hangman_challenge.py
@@ -1,88 +1,24 @@
 import random
 import time
-
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-    +---+
-    |   |
-    O   |
-   /|   |
-        |
-        |
-=========''', '''
-    +---+
-    |   |
-    O   |
-    |   |
-        |
-        |
-=========
-''', '''
-    +---+
-    |   |
-    O   |
-        |
-        |
-        |
-=========
-''', '''
-    +---+
-    |   |
-    |
-    |
-    |
-    |
-=========
-''']
+import hangman_logo_module
+from hangman_stages_module import stages
+from hangman_word_bank import word_bank
 
 
-
-# ******* using while testing code // comment out print later******
-# print(f"\n-----------------------\n Hidden word: {hidden_word} \n-----------------------\n ***Comment this line ^^ after testing is complete***\n") 
-
-logo = ''' 
- _                                             
-| |                                            
-| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
-| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
-| | | | (_| | | | | (_| | | | | | | (_| | | | |
-|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                    __/ |                      
-                   |___/    '''
-print(logo)
-time.sleep(1)
+hangman_logo_module
 
 print(f"\n--<3----------------------------------\n  Welcome to Hangman \n The Game of Guessing Words \n-------------------------------<3-----\n  ") 
 time.sleep(1)
 
-word_bank = ["able", "agility", "abroad", "blanket", "birds", "balloon", "passage", "wanderlust", "wayfaring", "voyage", "voyaging", "trekking", "expedition", "home", "London", "flat", "engineer"]
+# word_bank = ["able", "agility", "abroad", "blanket", "birds", "balloon", "passage", "wanderlust", "wayfaring", "voyage", "voyaging", "trekking", "expedition", "home", "London", "flat", "engineer"]
+
 hidden_word = random.choice(word_bank).lower()
 word_length = len(hidden_word)
 display = []
 lives = 6
+
+# ******* using while testing code // comment out print later******
+print(f"\n-----------------------\n Hidden word: {hidden_word} \n-----------------------\n ***Comment this line ^^ after testing is complete***\n") 
 
 
 for _ in range(word_length):
@@ -93,6 +29,9 @@ print(f"\n{display}")
 while "_" in display and lives > 0:
     user_guess = input(f"\nGuess a letter: ").lower()
 
+    if user_guess in display:
+        print(f"You've guessed {user_guess} already. \n")
+
     for position, letter in enumerate(hidden_word):
         if letter == user_guess:
             display[position] = user_guess
@@ -100,13 +39,14 @@ while "_" in display and lives > 0:
     if user_guess not in hidden_word:
         lives -= 1
         time.sleep(1)
-        print(f"{stages[lives]} \n\n Letter '{user_guess}' is not in the word.\n Lose a life: {lives}.\n\n")
+        print(f"\n Letter '{user_guess}' is not in the word.\n Lose a life: {lives}\n {stages[lives]}  \n")
 
     print(display)
 
 if "_" not in display and lives > 0:
     time.sleep(1)
     print(f"\nYou Win! The word is: {hidden_word}.\n")
+
 elif lives == 0:
     time.sleep(1)
     print(f"You lose, no lives left. \n The word was: {hidden_word}.\n")
